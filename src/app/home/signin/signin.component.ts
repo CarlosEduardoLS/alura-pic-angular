858d76ba3715824b885659a9c14e7fbd5e6cf040
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-15 15:43:42 -0400

    FEAT: Included auth-service on singin component and created login function

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -7,7 +8,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
     this.loginForm = formBuilder.group({});
   }
 
@@ -16,5 +20,18 @@ export class SignInComponent implements OnInit {
       userName: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.authenticate(userName, password).subscribe(
+      () => console.log('autenticado'),
+      (err) => {
+        console.log(err);
+        alert('Invalid Username or Password!');
+        this.loginForm.reset();
+      }
+    );
   }
 }
