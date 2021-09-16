a89e2074c562d549b3b84a387b3a629c4e39c279
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-16 16:17:24 -0300

    FEAT: implemented optimization of service use

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -15,6 +15,7 @@ import { UserNotTakenValidatorService } from './user-not-taken.validator.service
 
 @Component({
   templateUrl: './signup.component.html',
+  providers: [UserNotTakenValidatorService],
 })
 export class SignUpComponent implements OnInit, AfterViewInit {
   signupForm: FormGroup;
