f3f0e2e6eb779f57e6a44aba5d2b723d63be4a6f
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-16 10:08:49 -0300

    FEAT: Implemented logout function

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -11,11 +12,12 @@ export class HeaderComponent {
   user$: Observable<User | null>;
   user!: User;
 
-  constructor(userService: UserService) {
-    this.user$ = userService.getUser();
+  constructor(private userService: UserService, private router: Router) {
+    this.user$ = this.userService.getUser();
+  }
 
-    this.user$.subscribe((user) => {
-      if (user) this.user = user;
-    });
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
