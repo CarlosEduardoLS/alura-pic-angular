7064b8c8ecdea36b5c66584402d59d8c78675a68
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-17 12:08:44 -0300

    FEAT: Created new component

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo-details',
+  templateUrl: './photo-details.component.html',
+})
+export class PhotoDetailsComponent {
+  constructor() {}
+}
