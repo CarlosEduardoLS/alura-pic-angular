4162c847aff5296907e1f5dab7d3d9f59514bdfa
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-17 11:58:28 -0300

    FEAT: Included new dependency

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoFormComponent } from './photo-form.component';
 
@@ -15,6 +16,7 @@ import { PhotoFormComponent } from './photo-form.component';
     FormsModule,
     RouterModule,
     PhotoModule,
+    ImmediateClickModule,
   ],
 })
 export class PhotoFormModule {}
