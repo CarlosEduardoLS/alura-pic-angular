7a6de9f92f6f2a52e6e1366913e7fc6981c1326b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-17 11:07:29 -0300

    FEAT: Implemented upload function

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-form',
@@ -10,7 +12,11 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) {
     this.photoForm = formBuilder.group({});
     this.file = new File([], '');
   }
@@ -28,10 +34,10 @@ export class PhotoFormComponent implements OnInit {
   }
 
   upload() {
-    const description = this.photoForm.get('description')?.value;
-    const allowComments = this.photoForm.get('allowComments')?.value;
-    console.log(description);
-    console.log(allowComments);
-    console.log(this.file);
+    const description = this.photoForm.get('description')!.value;
+    const allowComments = this.photoForm.get('allowComments')!.value;
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
   }
 }
