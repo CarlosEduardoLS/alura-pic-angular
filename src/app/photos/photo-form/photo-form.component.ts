c6f47697003228bd3ee19ccfa68455a06df00852
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-17 09:03:30 -0300

    FEAT: Created functions to make html works

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ap-photo-form',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-form.component.css'],
 })
 export class PhotoFormComponent implements OnInit {
-  constructor() {}
+  photoForm: FormGroup;
+  file: File;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {
+    this.photoForm = formBuilder.group({});
+    this.file = new File([], '');
+  }
+
+  ngOnInit(): void {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
+  }
+
+  fileUpload(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  upload() {
+    const description = this.photoForm.get('description')?.value;
+    const allowComments = this.photoForm.get('allowComments')?.value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
+  }
 }
