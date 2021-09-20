8d67b44c75f4b23901c02f5edcf05c9671811720
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-20 10:19:24 -0300

    FEAT: Removed css not used

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
-import { PhotoComment } from '../photo/photo-comment';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-details',
   templateUrl: './photo-details.component.html',
-  styleUrls: ['photo-details.css'],
 })
 export class PhotoDetailsComponent implements OnInit {
   photo$: Observable<Photo | null>;
