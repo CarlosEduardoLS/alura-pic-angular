ac5bc5f9efe92e7ed84e4977be160c84622827df
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-20 10:20:24 -0300

    FEAT: Created new function to add commentary

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -11,10 +13,31 @@ import { PhotoService } from '../../photo/photo.service';
 export class PhotoCommentsComponent implements OnInit {
   @Input() photoId!: number;
   comments$: Observable<PhotoComment[]>;
+  commentForm: FormGroup;
 
-  constructor(private photoService: PhotoService) {
+  constructor(
+    private photoService: PhotoService,
+    private formBuilder: FormBuilder
+  ) {
     this.comments$ = this.photoService.getComments(this.photoId);
+    this.commentForm = this.formBuilder.group({});
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.maxLength(300)],
+    });
+  }
+
+  save() {
+    const comment = this.commentForm.get('comment')?.value as string;
+    this.comments$ = this.photoService
+      .addComment(this.photoId, comment)
+      .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+      .pipe(
+        tap(() => {
+          this.commentForm.reset();
+        })
+      );
+  }
 }
