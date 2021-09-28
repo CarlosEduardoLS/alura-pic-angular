12a8f1fd8142c0d9834b6197324b92854520cfaa
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-28 11:17:58 -0300

    FEAT: Included success message on upload image action

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) {
     this.photoForm = formBuilder.group({});
     this.file = new File([], '');
@@ -42,6 +44,9 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments')!.value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete');
+        this.router.navigate(['']);
+      });
   }
 }
