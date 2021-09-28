9da56726f94b7366017cb343c8df0ca5b0fb5606
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-28 10:12:08 -0300

    FEAT: Implemented new error message to remove photo action

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -15,7 +16,8 @@ export class PhotoDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) {
     this.photoId = this.route.snapshot.params.photoId;
     this.photo$ = this.photoService.findById(this.photoId);
@@ -26,8 +28,15 @@ export class PhotoDetailsComponent implements OnInit {
   }
 
   remove() {
-    this.photoService
-      .removePhoto(this.photoId)
-      .subscribe(() => this.router.navigate(['']));
+    this.photoService.removePhoto(this.photoId).subscribe(
+      () => {
+        this.alertService.success('Photo removed!');
+        this.router.navigate(['']);
+      },
+      (err) => {
+        console.log(err);
+        this.alertService.warning('Could not delete the photo!s');
+      }
+    );
   }
 }
