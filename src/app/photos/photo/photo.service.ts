6161d5f5c3cba9a1cbcee222cbb819c40fb8d4b5
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-30 17:22:35 -0300

    FEAT: Changed local api link to environment api link

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -4,8 +4,9 @@ import { of, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Photo } from './photo';
 import { PhotoComment } from './photo-comment';
+import { environment } from 'src/environments/environment';
 
-const API = 'http://localhost:3000';
+const API = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root',
